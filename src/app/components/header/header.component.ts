36a774a107bbc37b4caa6a064857afa9fe0286f5
luana.pereira <luana.pereira@memora.com.br>
2026-06-12 10:38:40 -0300

    design moderno

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  scrolled = false;
 
+  @HostListener('window:scroll')
+  onScroll() {
+    this.scrolled = window.scrollY > 60;
+  }
 }
